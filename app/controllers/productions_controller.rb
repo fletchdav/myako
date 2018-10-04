4106362e3757d7001fa187a7e31dc74f3069ed08
David Fletcher <fletchdav@gmail.com>
2018-10-04 16:51:04 +0200

    crud ok

--- app/controllers/productions_controller.rb
+++ app/controllers/productions_controller.rb
@@ -1,9 +1,31 @@
 class ProductionsController < ApplicationController
   skip_before_action :authenticate_user!
+  before_action :set_production, only: [:show, :edit, :update ]
 
   def index
     @productions = Production.all || []
     @labels = Label.all || []
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    @production.update(production_params)
+    redirect_to production_path(@production)
+  end
+
+  private
+
+  def production_params
+    params.require(:production).permit(:title)
+  end
+
+  def set_production
+    @production = Production.find(params[:id])
   end
 
 end
