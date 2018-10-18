bd8b4dff5eb18290cd88f245a7909c6ffd333b90
David Fletcher <fletchdav@gmail.com>
2018-10-18 21:09:58 +0200

    k

--- app/models/info.rb
+++ app/models/info.rb
@@ -1,6 +1,6 @@
 class Info < ApplicationRecord
   validates :category,
-    :inclusion  => { :in => [ 'contact', 'musique', 'reseau', 'presse', 'gig' ],
+    :inclusion  => { :in => [ 'contact', 'musique', 'reseau', 'presse', 'gig', 'collaboration' ],
     :message    => "Une de ces catégories doit être sélectionnée: Contact, Musique, Réseau, Presse, Gig" }
   validates :detail, presence: true
 end
